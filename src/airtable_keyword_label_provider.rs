38e6c1ebb6336eb8ed64996aa6f43fe95feaa761
Marcel Hamade <hamade.marcel@gmail.com>
2022-11-09 22:24:15 +0100

    fix label removal

--- src/airtable_keyword_label_provider.rs
+++ src/airtable_keyword_label_provider.rs
@@ -1,41 +1,40 @@
 pub mod airtable_keyword_label_provider {
-    use serde_derive::{Serialize, Deserialize};
+    use crate::KeywordLabelCombo;
+    use serde_derive::{Deserialize, Serialize};
 
-    use crate::{KeywordLabelCombo};
-   
     #[derive(Debug, Serialize, Deserialize)]
     pub struct AirtableResponse {
-        records: Records
+        records: Records,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     #[serde(transparent)]
     pub struct Records {
-        array: Vec<AirtableEntries>
+        array: Vec<AirtableEntries>,
     }
 
     #[derive(Debug, Serialize, Deserialize, Clone)]
     pub struct AirtableEntries {
-        fields: AirtableKeywordLabel
+        fields: AirtableKeywordLabel,
     }
 
     #[derive(Debug, Serialize, Deserialize, Clone)]
-    pub struct AirtableKeywordLabel{
+    pub struct AirtableKeywordLabel {
         #[serde(default)]
         #[serde(rename = "Keyword")]
         keyword: String,
         #[serde(default)]
         #[serde(rename = "Label")]
-        label: String
+        label: String,
     }
 
-    impl From<AirtableKeywordLabel> for KeywordLabelCombo{
+    impl From<AirtableKeywordLabel> for KeywordLabelCombo {
         fn from(a: AirtableKeywordLabel) -> Self {
             Self {
-              keyword: a.keyword,
-              label: a.label
+                keyword: a.keyword,
+                label: a.label,
             }
-          }
+        }
     }
 
     pub async fn get_keyword_label_combos(
@@ -43,8 +42,8 @@ pub mod airtable_keyword_label_provider {
         airtable_token: &str,
     ) -> Vec<KeywordLabelCombo> {
         let client: reqwest::Client = reqwest::Client::new();
-        
-        // TODO: 100 values max -> if more use pagination
+
+        // TODO: 100 values max -> if we have more we need to use use pagination
         let response = client
             .get(airtable_url)
             .header("Authorization", "Bearer ".to_owned() + &airtable_token)
@@ -54,15 +53,19 @@ pub mod airtable_keyword_label_provider {
         match response {
             Ok(res) => {
                 let airtable_response = res.json::<AirtableResponse>().await.unwrap();
-                let result : Vec<KeywordLabelCombo> = airtable_response.records.array
+                let result: Vec<KeywordLabelCombo> = airtable_response
+                    .records
+                    .array
                     .iter()
-                    .map(|a| KeywordLabelCombo{ keyword: String::from(&a.fields.keyword), label: String::from(&a.fields.label)})
+                    .map(|a| KeywordLabelCombo {
+                        keyword: String::from(&a.fields.keyword),
+                        label: String::from(&a.fields.label),
+                    })
                     .collect();
                 result
-
             }
-            Err(e) =>{
-                println!("Error retrieving Airtable data: {:?}",e);
+            Err(e) => {
+                println!("Error retrieving Airtable data: {:?}", e);
                 return Vec::<KeywordLabelCombo>::with_capacity(1);
             }
         }
